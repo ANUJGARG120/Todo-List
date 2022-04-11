990f7a264dd29ce13a589fbebfea5e0f8766a8c7
Anuj Kumar garg <94665437+ANUJGARG120@users.noreply.github.com>
2022-04-11 12:07:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style.css'
 
-function App() {
+
+let nextId = 0;
+const App = () => {
+  const [value, setValue] = useState('');
+  const [list, setList] = useState([]);
+
+
+  const listHandler = () => {
+    setValue('');
+    if (value.length > 0) {
+      setList([
+        ...list,
+        {
+          id: nextId++,
+          data: value,
+        },
+      ]);
+    } else {
+      alert('data is not found');
+    }
+  };
+  const valueHandler = (e) => {
+    setValue(e.target.value);
+  };
+  const deleteHandler = (id) => {
+    let newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="todo">
+      <h1 className="h1">Todo List</h1>
+      <input className="yes" type="text" value={value} onChange={valueHandler} />
+      <button className="btn" onClick={listHandler}>Add</button>
+      
+    </div>
+     <div className="ui" >
+   
+        {list.map((item) => (
+         <div className='li'>
+            <li  key={item.id}>
+            {item.data}
+            <button className="dlt" onClick={() => deleteHandler(item.id)}>X</button>
+          </li>
+         </div>
+        ))}
+      
+     </div>
     </div>
   );
-}
-
+};
 export default App;
